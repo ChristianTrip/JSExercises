5baeaaed80d0021f9d79c3d17464a1a46792f2ed
Christian Sandholdt Trip <90309082+ChristianTrip@users.noreply.github.com>
2022-09-26 23:43:49 +0200

    Add files via upload

---
+++ resources/scripts/day1.js
@@ -0,0 +1,12 @@
+
+// setup
+    const day1 = document.getElementById("day1-btn");
+    
+    day1.style.backgroundColor = "white";
+    day1.style.color = "black";
+    day1.style.border = "2px solid #333";
+    day1.style.fontWeight = 400;
+
+
+
+
